e8d9c86bcef16a1c2154e110aac33a239cdd25dd
Hans Landgrebe <hlxbl20@gmail.com>
2026-07-22 13:00:31 -0500

    feat: inital commit

---
+++ Downkeep/Assets/Scripts/Input/PlayerInputManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    [SerializeField] PlayerInput input;
+
+    private void Start()
+    {
+        
+    } 
+
+    private void MoveInput()
+    {
+        
+    }
+}
---
+++ Downkeep/Assets/Scripts/Input/Puppet.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Puppet : MonoBehaviour
+{
+    public void MoveAction(Vector2 moveVector)
+    {
+        
+    }
+
+    public void JumpAction()
+    {
+        
+    }
+
+    public void AttackAction()
+    {
+        
+    }
+
+    public void AltAttackAction()
+    {
+        
+    }
+}

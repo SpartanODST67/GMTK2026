bb91bd51efb85ae55a48ea4959cbd98b43ed0416
Hans Landgrebe <hlxbl20@gmail.com>
2026-07-22 15:34:47 -0500

    feat: cursor

---
+++ Downkeep/Assets/Scripts/Cursor/Cursor.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum CursorState
+{
+    Default,
+    Pointer,
+    Targeter
+}
+
+public class Cursor : MonoBehaviour
+{
+    [SerializeField] SerializableDictionary<CursorState, Sprite> cursorSprites;
+    private Dictionary<CursorState, Sprite> spriteDict;
+    [SerializeField] SpriteRenderer sprite;
+    Camera cam;
+
+    void Awake()
+    {
+        spriteDict = cursorSprites.ToDict();
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 pointerPos = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        pointerPos.z = -9;
+        gameObject.transform.position = pointerPos;
+    }
+
+    public void SetCursorState(CursorState state)
+    {
+        sprite.sprite = spriteDict[state] ?? spriteDict[CursorState.Default];
+    }
+}

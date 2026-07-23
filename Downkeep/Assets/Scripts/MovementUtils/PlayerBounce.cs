ef5e82b03fde440e0374bcd3b3c435697f1fb784
Hans Landgrebe <hlxbl20@gmail.com>
2026-07-22 19:12:29 -0500

    feat: player bounce

---
+++ Downkeep/Assets/Scripts/MovementUtils/PlayerBounce.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerBounce : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] PlayerPuppet playerPuppet;
+    [SerializeField] int bounceLength;
+
+    public void Bounce(Vector3 force)
+    {
+        StartCoroutine(BounceRoutine(force));
+    }
+
+    IEnumerator BounceRoutine(Vector3 force)
+    {
+        playerPuppet.isBouncing = true;
+        yield return null;
+
+        if(rb.linearVelocityY < 0)
+            rb.linearVelocityY = 0;
+        
+        rb.AddForce(force, ForceMode2D.Impulse);
+
+        for(int i = 0; i < bounceLength; i++)
+            yield return new WaitForSecondsRealtime(1/60);
+
+        playerPuppet.isBouncing = false;        
+    }
+
+}

58aa3bfde7ccb9ddbed9020a2660f690c3e655f4
Hans Landgrebe <hlxbl20@gmail.com>
2026-07-23 13:18:07 -0500

    refactor: player bounce

--- Downkeep/Assets/Scripts/MovementUtils/PlayerBounce.cs
+++ Downkeep/Assets/Scripts/MovementUtils/PlayerBounce.cs
@@ -5,27 +5,17 @@ public class PlayerBounce : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] PlayerPuppet playerPuppet;
-    [SerializeField] int bounceLength;
 
     public void Bounce(Vector3 force)
     {
-        StartCoroutine(BounceRoutine(force));
-    }
-
-    IEnumerator BounceRoutine(Vector3 force)
-    {
-        playerPuppet.canMove = false;
-        yield return null;
-
         if(rb.linearVelocityY < 0)
             rb.linearVelocityY = 0;
-        
+
+        if(rb.linearVelocityX < 0 && force.x > 0 || rb.linearVelocityX > 0 && force.x < 0)
+        {
+            rb.linearVelocityX = 0;
+        }
+
         rb.AddForce(force, ForceMode2D.Impulse);
-
-        for(int i = 0; i < bounceLength; i++)
-            yield return new WaitForSecondsRealtime(1/60);
-
-        playerPuppet.canMove = true;        
     }
-
 }

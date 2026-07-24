8c9db74eaee3aae85d0d473f736f07a88f9189c8
Hans Landgrebe <hlxbl20@gmail.com>
2026-07-23 20:12:47 -0500

    feat: vampire hunter states

--- Downkeep/Assets/Scripts/Enemy/Vampire Hunter/VampireHunterBrain.cs
+++ Downkeep/Assets/Scripts/Enemy/Vampire Hunter/VampireHunterBrain.cs
@@ -1,5 +1,12 @@
 using UnityEngine;
 
+enum VampireHunterState
+{
+    Walking,
+    Aiming,
+    Recovering
+}
+
 public class VampireHunterBrain : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
@@ -9,15 +16,39 @@ public class VampireHunterBrain : MonoBehaviour
     [SerializeField] BoxCollider2D edgeDetection;
     [SerializeField] BoxCollider2D wallDetection;
 
+    [SerializeField] Vector2 walkTimeRange;
+    float walkTime = 0;
+    [SerializeField] float aimTime;
+    [SerializeField] float attackRecoveryTime;
+
+    [SerializeField] VampireHunterState state;
+    float timeInState = 0;
+
     void Start()
     {
         if(randomizeDirection)
             direction = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        walkTime = Random.Range(walkTimeRange.x, walkTimeRange.y);
     }
 
     void Update()
     {
-        rb.linearVelocityX = walkSpeed * direction;
+        switch(state)
+        {
+            default:
+            case VampireHunterState.Walking:
+                rb.linearVelocityX = walkSpeed * direction;
+                break;
+            case VampireHunterState.Aiming:
+                AimWeaponState();
+                break;
+            case VampireHunterState.Recovering:
+                RecoverState();
+                break;
+        }
+
+        EvaluateStateTime();
     }
 
     public void ChangeWalkDirection()
@@ -25,5 +56,58 @@ public class VampireHunterBrain : MonoBehaviour
         direction *= -1;
         edgeDetection.offset = new Vector2(-edgeDetection.offset.x, edgeDetection.offset.y);
         wallDetection.offset = new Vector2(-wallDetection.offset.x, wallDetection.offset.y);
+    }
+
+    private void EvaluateStateTime()
+    {
+        timeInState += Time.deltaTime;
+
+        float expectedTime = state switch
+        {
+            VampireHunterState.Walking => walkTime,
+            VampireHunterState.Aiming => aimTime,
+            VampireHunterState.Recovering => attackRecoveryTime,
+            _ => 0
+        };
+
+        if(timeInState >= expectedTime)
+            NextState();
+    }
+
+    private void NextState()
+    {
+        switch(state)
+        {
+            case VampireHunterState.Walking:
+                state = VampireHunterState.Aiming;
+                break;
+            case VampireHunterState.Aiming:
+                Shoot();
+                state = VampireHunterState.Recovering;
+                break;
+            case VampireHunterState.Recovering:
+                walkTime = Random.Range(walkTimeRange.x, walkTimeRange.y);
+                state = VampireHunterState.Walking;
+                break;
+        }
+
+        timeInState = 0;
+    }
+
+    private void AimWeaponState()
+    {
+        rb.linearVelocityX = 0;
+        Debug.Log("Aiming");
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Bang");
+    }
+
+    private void RecoverState()
+    {
+        rb.linearVelocityX = 0;
+        Debug.Log("Recovering");
     }
 }
